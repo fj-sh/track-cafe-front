bc1567b233108f82ff94c8f6466f770ec674deda
fj <menspocky322@gmail.com>
2022-05-29 09:36:22 +0900

    全体のレイアウトを整えた

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,8 +4,7 @@ import { render } from '@testing-library/react'
 import App from './App'
 
 describe('<App /> のスナップショットテスト', () => {
-  // TODO: Vitest で updateSnapshot する方法が 2022-05-28 時点で不明。とはいえコンポーネント数が少ないため、一旦 skip でしのぐ
-  it.skip('<App /> がレンダリングされて表示されること', () => {
+  it('<App /> がレンダリングされて表示されること', () => {
     const { container } = render(<App />)
     expect(container.firstChild).toMatchSnapshot()
   })
